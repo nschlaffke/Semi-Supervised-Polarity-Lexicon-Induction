80aa151fa2099ed6b77faffad262f60e95f4a2d7
Norbert Schlaffke <norbert.schlaffke@student.put.poznan.pl>
2020-01-05 20:55:03 +0100

    Optimize labelProp

--- labelProp.py
+++ labelProp.py
@@ -1,34 +1,37 @@
 import numpy as np
+from scipy import sparse
 
-def performLabelProp(graph, seeds1, seeds2):
+def performLabelProp(graph, seeds1, seeds2, n_iter=10000):
     LABELS = 3 # 2 classes + default class
     s1_id = [graph.vs().find(word).index for word in seeds1]
     s2_id = [graph.vs().find(word).index for word in seeds2]
 
     # Generate trainsition matrix
     W = getW(graph)
-    row_sums = W.sum(axis=1)
-    T = W / row_sums[:, np.newaxis]
-    T = np.nan_to_num(T, 0) # TODO nans are converted to 0 because of vertices with no connections
-
+    row_sums = W.sum(axis=1, keepdims=True)
+    row_sums[row_sums==0] = 1
+    T = W / row_sums
+    del W
+    del row_sums    
+    T = sparse.csr_matrix(T)
     # Step 1 - initialize
     V = len(graph.vs())
     Y = np.zeros((V, LABELS))
     Y[:,2] = 1 # Set all nodes to default label
-    for _ in range(100):
-        for s_id in s1_id:
-            Y[s_id,:] = np.array([1,0,0])
-        for s_id in s2_id:
-            Y[s_id,:] = np.array([0,1,0])
+    for _ in range(n_iter):
+        print(_)
+        Y[s1_id] = np.array([1,0,0])
+        Y[s2_id,:] = np.array([0,1,0])
 
         # Step 2 - propagate
         Y = T@Y
         
         # Step 3 - row-normalize
-        row_sums = Y.sum(axis=1)
-        Y /= row_sums[:, np.newaxis]
-        Y = np.nan_to_num(Y, 0) # TODO nans are converted to 0 because of vertices with no connections
-    return Y
+        row_sums = Y.sum(axis=1, keepdims=True)
+        row_sums[row_sums==0] = 1
+        Y /= row_sums
+        
+    return np.argmax(Y, axis=1)
 
 def getW(graph, undirected=True):
     V = len(graph.vs())
