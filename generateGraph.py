fe8b5f6cc46ef26258b88f4cfd65f9b63114c81e
Martin <martin.dansc@gmail.com>
2020-01-11 17:32:11 +0100

    added cache graph

--- generateGraph.py
+++ generateGraph.py
@@ -1,4 +1,5 @@
 from nltk.corpus import wordnet as wn
+from os import path
 import igraph as ig
 
 import graphFunctions
@@ -9,7 +10,32 @@ import labelProp
 from sklearn.externals.joblib import Memory
 memory = Memory(location='./cache', verbose=0)
 
-def getADJGraph(words_synsets):
+# Graph cache functions
+def graphPath(name):
+    return "graphs/" + name + ".graphml"
+
+def storeGraph(name, g):
+    g.save(graphPath(name))
+    
+def readGraph(name):
+    if(path.exists(graphPath(name))):
+        return ig.load(graphPath(name))
+    return None
+
+def graphCache(function):
+    def new_function():
+        stored_graph = readGraph(function.__name__)
+        if(stored_graph != None):
+            return stored_graph
+        g = function()
+        storeGraph(function.__name__, g)
+        return g
+
+    return new_function
+
+ # Graph functions   
+
+def getGraph(words_synsets):
     lemmas = loadWordNet.getAllLemmas(words_synsets)
 
     graph = graphFunctions.createGraph(loadWordNet.getNames(lemmas))
@@ -25,23 +51,30 @@ def getADJGraph(words_synsets):
     graph.es()['weight'] = [1]*len(graph.get_edgelist()) # TODO remove dummy way of adding weights
     return graph
 
-@memory.cache
+@graphCache
 def getFullADJGraph():
     # possible word types: ADJ, ADJ_SAT, ADV, NOUN, VERB
     words_synsets = list(wn.all_synsets(wn.ADJ))
-    return getADJGraph(words_synsets)
+    return getGraph(words_synsets)
 
-    
-
-@memory.cache
+@graphCache
 def getSmallADJGraph():
+    name = "smallAdjGraph"
+    storedGraph = readGraph(name)
+    if(storedGraph != None):
+        return storedGraph
+
     # possible word types: ADJ, ADJ_SAT, ADV, NOUN, VERB
     words_synsets = list(wn.all_synsets(wn.ADJ))[:500]
-    return getADJGraph(words_synsets)
+    return getGraph(words_synsets)
+
+
+# Main for tests
 
 if __name__ == "__main__":
     # memory.clear(warn=False)
     g = getFullADJGraph()
     # ig.plot(g)
-    print(graphFunctions.shortestPath(g, "good", "bad"))
-    graphPlots.plotWithLabels(g.subgraph(graphFunctions.shortestPath(g, "good", "bad")[0]))
+    # print(graphFunctions.shortestPath(g, "good", "bad"))
+    # graphPlots.plotWithLabels(g.subgraph(graphFunctions.shortestPath(g, "good", "bad")[0]))
+    # storeGraph("fullAdjGraph", g)
--- graphPlots.py
+++ graphPlots.py
@@ -4,5 +4,8 @@ def plotWithLabels(graph):
     graph.vs()["label"] = graph.vs()["name"]
     ig.plot(graph)
 
+def simplePlot(graph):
+    ig.plot(graph)
+
 def plotCommunity(communities):
     ig.plot(communities, mark_groups = True)
