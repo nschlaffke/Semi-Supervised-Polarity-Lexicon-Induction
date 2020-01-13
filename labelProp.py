aca370f0eea2c8822612b08c4b87321caa4ce672
Norbert Schlaffke <norbert.schlaffke@student.put.poznan.pl>
2020-01-13 08:14:34 +0100

    Get seeds and calculate scores for labelProp

--- labelProp.py
+++ labelProp.py
@@ -1,10 +1,20 @@
 import numpy as np
 from scipy import sparse
+from graphFunctions import getVerticesNames
+
+def getIndex(graph, word):
+    try:
+       return graph.vs().find(word).index
+    except ValueError:
+        return -1
 
 def performLabelProp(graph, seeds1, seeds2, n_iter=10000):
     LABELS = 3 # 2 classes + default class
-    s1_id = [graph.vs().find(word).index for word in seeds1]
-    s2_id = [graph.vs().find(word).index for word in seeds2]
+    s1_id = [getIndex(graph, word) for word in seeds1]
+    s2_id = [getIndex(graph, word) for word in seeds2]
+
+    s1_id = [x for x in s1_id if x != -1]
+    s2_id = [x for x in s2_id if x != -1]
 
     # Generate trainsition matrix
     W = getW(graph)
@@ -19,7 +29,6 @@ def performLabelProp(graph, seeds1, seeds2, n_iter=10000):
     Y = np.zeros((V, LABELS))
     Y[:,2] = 1 # Set all nodes to default label
     for _ in range(n_iter):
-        print(_)
         Y[s1_id] = np.array([1,0,0])
         Y[s2_id,:] = np.array([0,1,0])
 
@@ -31,7 +40,10 @@ def performLabelProp(graph, seeds1, seeds2, n_iter=10000):
         row_sums[row_sums==0] = 1
         Y /= row_sums
         
-    return np.argmax(Y, axis=1)
+    labels = np.argmax(Y, axis=1)
+    positives = np.where(labels==0)[0]
+    negatives = np.where(labels==1)[0]
+    return getVerticesNames(graph, positives), getVerticesNames(graph, negatives)
 
 def getW(graph, undirected=True):
     V = len(graph.vs())
--- main.py
+++ main.py
@@ -3,14 +3,21 @@
 # nltk.download('wordnet')
 from nltk.corpus import wordnet as wn
 import igraph as ig
+import numpy as np
 
 import graphFunctions
 import generateGraph
 import score
 import helper
+import labelProp
+
+NUMBER_OF_SEEDS = 100
 
 real_positives = helper.readCsvWords("./LMDictCsv/LMDpositive.csv")
 real_negatives = helper.readCsvWords("./LMDictCsv/LMDnegative.csv")
+
+negative_seed = np.random.choice(real_negatives, NUMBER_OF_SEEDS)
+positive_seed = np.random.choice(real_positives, NUMBER_OF_SEEDS)
 
 graph = generateGraph.getFullADJGraph()
 (predicted_positives, predicted_negatives) = graphFunctions.getMinCut(graph, "good", "bad")
@@ -21,4 +28,16 @@ fscores.append(score.correctedFscore("Corrected Fscore", real_positives, real_ne
 
 score.saveFScoresTable(fscores, "good")
 score.printFscores(fscores)
-# label_prop = labelProp.performLabelProp(graph, ['abaxial'], ['abducent'])
+
+# Label prop
+print("Label propagation")
+(predicted_positives, predicted_negatives) = labelProp.performLabelProp(
+    graph, 
+    ["good"], 
+    ["bad"])
+fscores = []
+fscores.append(score.fscore("Simple Fscore", real_positives, real_negatives, predicted_positives, predicted_negatives))
+fscores.append(score.correctedFscore("Corrected Fscore", real_positives, real_negatives, predicted_positives, predicted_negatives, graph.vs()["name"]))
+
+score.saveFScoresTable(fscores, "good")
+score.printFscores(fscores)
