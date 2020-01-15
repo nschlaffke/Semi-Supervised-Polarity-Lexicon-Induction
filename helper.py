6a2c31abb640fe618f6cd8b668c27eb024a1bec7
Norbert Schlaffke <norbert.schlaffke@student.put.poznan.pl>
2020-01-15 22:13:15 +0100

    Implement getSeed

--- helper.py
+++ helper.py
@@ -22,3 +22,19 @@ def readCsvWords(filepath):
                 readed.append(row[1].lower())
             line_count += 1
     return readed 
+
+def getSeed(graph, size, real_positives, real_negatives):
+    """
+        Return the same words for same graph and same
+    """
+    if size == 2 and 'good' in real_positives and 'bad' in real_negatives:
+        return ['good'], ['bad']
+
+    np.random.seed(size)
+    used_dictionary = graphFunctions.getVerticesNames(
+        graph, range(len(graph.vs())))
+
+    chosen_pos = np.random.choice(real_positives, size=size)
+    chosen_neg = np.random.choice(real_negatives, size=size)
+
+    return chosen_pos.tolist(), chosen_neg.tolist()
