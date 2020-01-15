f0766e2547c3b6c399fbebf54456ce4cc4cc3386
Norbert Schlaffke <norbert.schlaffke@student.put.poznan.pl>
2020-01-15 03:00:24 +0100

    Use new scoring, filter real pos/neg, delete nodes of 0 deg from graph

--- main.py
+++ main.py
@@ -11,57 +11,63 @@ import score
 import helper
 import labelProp
 import minCut
+import newScore
+
 
 def performMinCut(graph, real_positives, real_negatives, fscores):
 
     # Use the simplest min cut
-    (predicted_positives, predicted_negatives) = minCut.getSimpleMinCut(graph, "good", "bad")
+    (predicted_positives, predicted_negatives) = minCut.getSimpleMinCut(
+        graph, "good", "bad")
 
-    fscores.append(score.correctedFscore("Simple min-cut", real_positives, real_negatives, predicted_positives, 
-        predicted_negatives, graph.vs()["name"]))
+    fscores.append(newScore.getScores("Simple min-cut", real_positives, real_negatives,
+                       predicted_positives, predicted_negatives))
 
+    
     # Try to improve min-cut, adding a high capacity to adjacent edges, so they don't get cut
-    (predicted_positives, predicted_negatives) = minCut.getNonNeighboursEdgesMinCut(graph, "good", "bad")
+    (predicted_positives, predicted_negatives) = minCut.getNonNeighboursEdgesMinCut(
+        graph, "good", "bad")
 
-    fscores.append(score.correctedFscore("Non Neighbours min-cut", real_positives, real_negatives, predicted_positives, 
-        predicted_negatives, graph.vs()["name"]))
+    fscores.append(newScore.getScores("Non Neighbours min-cut", real_positives, real_negatives,
+                       predicted_positives, predicted_negatives))                   
+
 
     # Try to improve min-cut, adding a subgraph of connected edges so they don't get cut
-    (predicted_positives, predicted_negatives) = minCut.getNonNeighboursEdgesMinCut(graph, "good", "bad")
+    (predicted_positives, predicted_negatives) = minCut.getNonNeighboursEdgesMinCut(
+        graph, "good", "bad")
 
-    fscores.append(score.correctedFscore("Non Neighbours min-cut", real_positives, real_negatives, predicted_positives, 
-        predicted_negatives, graph.vs()["name"]))
+    fscores.append(score.correctedFscore("Non Neighbours min-cut", real_positives, real_negatives, predicted_positives,
+                                         predicted_negatives, graph.vs()["name"]))
 
-    # Try with only the words 
+    # Try with only the words
+
 
 def performLabelProp(graph, real_positives, real_negatives, fscores):
-    # Label prop
+    NUMBER_OF_SEEDS = 50
     print("Label propagation")
+    negative_seed = np.random.choice(real_negatives, NUMBER_OF_SEEDS)
+    positive_seed = np.random.choice(real_positives, NUMBER_OF_SEEDS)
+
     (predicted_positives, predicted_negatives) = labelProp.performLabelProp(
-        graph, 
-        ["good"], 
-        ["bad"])
+        graph,
+        negative_seed,
+        positive_seed)
 
-    fscores.append(score.fscore("S LP", real_positives, real_negatives, predicted_positives, predicted_negatives))
-    fscores.append(score.correctedFscore("C LP", real_positives, real_negatives, predicted_positives, 
-        predicted_negatives, graph.vs()["name"]))
-
+    fscores.append(newScore.getScores("LP", real_positives, real_negatives,
+                       predicted_positives, predicted_negatives))
 
 # MAIN
-
-NUMBER_OF_SEEDS = 100
-
-real_positives = helper.readCsvWords("./LMDictCsv/LMDpositive.csv")
-real_negatives = helper.readCsvWords("./LMDictCsv/LMDnegative.csv")
-
-negative_seed = np.random.choice(real_negatives, NUMBER_OF_SEEDS)
-positive_seed = np.random.choice(real_positives, NUMBER_OF_SEEDS)
-
 graph = generateGraph.getFullADJGraph()
+graph = graphFunctions.removeDisconnectedVertices(graph)
+
+used_dictionary = graphFunctions.getVerticesNames(
+    graph, range(len(graph.vs())))
+real_positives = [word for word in helper.readCsvWords("./LMDictCsv/LMDpositive.csv")
+                  if word in used_dictionary]
+real_negatives = [word for word in helper.readCsvWords("./LMDictCsv/LMDnegative.csv")
+                  if word in used_dictionary]
+
 fscores = []
 
 performMinCut(graph, real_positives, real_negatives, fscores)
 performLabelProp(graph, real_positives, real_negatives, fscores)
-
-score.saveFScoresTable(fscores, "good")
-score.printFscores(fscores)
